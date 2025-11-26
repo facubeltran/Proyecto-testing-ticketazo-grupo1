b1c930de8bc14171afdb3ada4d2a94fb7c555c7a
facubeltran <facundocristianbeltran@gmail.com>
2025-11-26 14:15:58 -0300

    Caso 6,7, el 7 problemas con capturar la ventana de reserva vencio, y caso 9 solucion a validar que se abre una pestaña y que coincida con mercado.pago

--- cypress/e2e/Compra/happyPath.cy.js
+++ cypress/e2e/Compra/happyPath.cy.js
@@ -64,7 +64,7 @@ describe('Compras con Login', () => {
         cy.contains('Asientos').should('contain', 'Asientos seleccionados: 2');
     })
 
-    it.only('Caso 6: Seleccion de asientos disponibles(butacas)', () => {
+    it('Caso 6: Chequear disponibilidad por fecha', () => {
         cy.get('[data-cy="btn-ver-evento-9"]').click()
         cy.contains('Adquirir entrada').click().wait(1000)
         cy.contains('span', 'sábado, 21 de junio de 2025').click('top');
@@ -79,7 +79,59 @@ describe('Compras con Login', () => {
     })
     // no pude hacerlos con las otras fechas por un tema de diseño, no se si se superponian los div's que no agarraba el del sabado 14, creo que se superponia por el efecto ese de la card con el domingo 15
     // asi que agarre el ultimo para que pase la prueba, despues veo si lo puedo arreglar
-    
+    // hay que cambiar el test case
 
+    it('Caso 7: Reserva fuera de tiempo', () => {
+        const ahora = new Date();
+
+        cy.get('[data-cy="btn-ver-evento-9"]').click()
+        cy.contains('Adquirir entrada').click()
+        cy.contains('span', 'sábado, 21 de junio de 2025').click('top');
+        cy.contains('23:00').click()
+        cy.contains('Continuar con la compra').click()
+        cy.contains('Audiotorio').click()
+        cy.clock(ahora.getTime());
+        cy.log(ahora.getTime());
+        cy.get('button[title="Fila 5, Columna 7"]').click()
+        cy.get('button[title="Fila 5, Columna 8"]').click()
+        cy.contains('Tu reserva vence en:').should('be.visible')
+        cy.tick(300000)
+        cy.on('window:alert', (textoAlerta) => {
+            expect(textoAlerta).to.contains('Tu reserva ha vencido');
+        });
+        cy.contains('Comprar').click()
+        // cy.contains('Tu reserva ha vencido. Por favor, vuelve a seleccionar tus entradas.').should('be.visible')
+
+    })
+    it('Caso 8: Valor correcto del precio total', () => {
+        cy.get('[data-cy="btn-ver-evento-2"]').click()
+        cy.contains('Adquirir entrada').click()
+        cy.contains('Con Butacas').click()
+        cy.get('button[title="Fila 5, Columna 18"]').click().wait(2000)
+        cy.get('button[title="Fila 5, Columna 19"]').click().wait(2000)
+        cy.contains('Comprar').click()
+        cy.contains('2 entradas').should('be.visible')
+        cy.contains('Precio base: $5000')
+        cy.contains('$400.00 (8% del valor base)').should('be.visible')
+        cy.get('div').should('be.visible').and('contain', '$5400')
+
+    })
+
+    it.only('Caso 9: Compra de entrada', () => {
+        cy.get('[data-cy="btn-ver-evento-2"]').click()
+        cy.contains('Adquirir entrada').click()
+        cy.contains('Con Butacas').click()
+        cy.get('button[title="Fila 5, Columna 18"]').click().wait(1000)
+        cy.get('button[title="Fila 5, Columna 19"]').click().wait(1000)
+        cy.contains('Comprar').click()
+        cy.get('input').check({ force: true })
+        cy.window().then((win) => {
+            // 1. Creamos el espía (stub) sobre la función 'open'
+            cy.stub(win, 'open').as('abrir');
+        });
+         cy.contains('Pagar').click()
+         cy.get('@abrir').should('be.calledWith', Cypress.sinon.match('mercadopago.com'));
+    })
 })
+
 
