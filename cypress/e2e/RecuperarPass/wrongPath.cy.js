16461e1c3c75a7b19b5e602ba0e0ac503fa32931
AkoMerla <shaviitoo@gmail.com>
2025-11-26 02:52:09 -0300

    Se agregan Español e Inglés en su totalidad
    - Se agregan assert para los siguientes idiomas (todos verificados):
    . Español
    . Español (España)
    . Español (Argentina)
    . Español (Latinoamérica)
    . Español (Estados Unidos)
    . Inglés (Estados Unidos)
    . Inglés (Reino Unido, OXENDICT)
    . Inglés
    . Inglés (United Kingdom)
    
    - Idiomas a agregar en el próximo parche:
    . Turco-peruano
    . Germano-tucumano
    . Chileno (Cuático)

--- cypress/e2e/RecuperarPass/wrongPath.cy.js
+++ cypress/e2e/RecuperarPass/wrongPath.cy.js
@@ -13,17 +13,17 @@ viewports.forEach((vp)=>{
         it('sinTextoPostArroba',()=>{
             cy.get('[data-cy="input-email"]').type('asd@').blur()
             cy.get('[data-slot="error-message"]').invoke('text')
-            .should('match',/Introduce texto detrás del signo "@"|Please enter a part following '@'/)
+            .should('match',/Introduce texto detrás del signo "@"|Please enter a part following '@'|Ingresa texto después del signo "@"/)
         })
         it('simboloPostArroba',()=>{
             cy.get('[data-cy="input-email"]').type('test@=.com').blur()
             cy.get('[data-slot="error-message"]').invoke('text')
-            .should('match',/El texto detrás del signo "@" no debe incluir el símbolo|A part following '@' should not contain the symbol/)
+            .should('match',/El texto detrás del signo "@" no debe incluir el símbolo|A part following '@' should not contain the symbol|El texto después del signo "@" no debe incluir el símbolo/)
         })
         it('puntoPostArroba',()=>{
             cy.get('[data-cy="input-email"]').type('a@.').blur()
             cy.get('[data-slot="error-message"]').invoke('text')
-            .should('match',/El signo "." está colocado en una posición incorrecta en la dirección|'.' is used at a wrong position in/)
+            .should('match',/El signo "." está colocado en una posición incorrecta en|'.' is used at a wrong position in/)
         })
     })
 })
