435e3c0a868b5ec0c95efb4505e5465e28d4ffef
facubeltran <facundocristianbeltran@gmail.com>
2025-11-27 16:48:05 -0300

    Actualizados los test Cases, donde se automatiza la busqueda de asientos/lugares, y se agrego el viewport para mac15 y iphone 6

--- cypress/e2e/Compra/happyPath.cy.js
+++ cypress/e2e/Compra/happyPath.cy.js
@@ -1,175 +1,201 @@
-const viewports=['macbook-15','iphone-6']
-viewports.forEach((vp)=>{
+const viewports = ['macbook-15', 'iphone-6']
+viewports.forEach((vp) => {
     describe(`Compras con login:${vp}`, () => {
-  beforeEach(() => {
-    cy.visit("https://ticketazo.com.ar/auth/login");
-    cy.fixture("compra").then((data) => {
-      cy.get('[data-cy="input-email"]').type(data[0].emailExistente);
-      cy.get('[data-cy="input-password"]').type(data[0].password);
-      cy.get('[data-cy="btn-login"]').click();
+        let lugaresParaLimpiar = [];
+
+        beforeEach(() => {
+            cy.visit("https://ticketazo.com.ar/auth/login");
+            cy.fixture("compra").then((data) => {
+                cy.get('[data-cy="input-email"]').type(data[0].emailExistente);
+                cy.get('[data-cy="input-password"]').type(data[0].password);
+                cy.get('[data-cy="btn-login"]').click();
+            });
+        });
+
+        afterEach(() => {
+            if (lugaresParaLimpiar.length > 0) {
+                cy.log('Limpiando asientos seleccionados...');
+                lugaresParaLimpiar.forEach((info) => {
+                    cy.log('Intentando deseleccionar: ' + info)
+
+                    cy.get(`button[title="${info}"]`).click({ force: true });
+                });
+                // Vaciamos el array para que no intente limpiar en el siguiente test si no compró nada
+                lugaresParaLimpiar = [];
+            }
+        });
+
+        it("Caso 1: Chequear disponibilidad de lugares", () => {
+            cy.get('[data-cy="btn-ver-evento-8"]').click();
+            cy.contains("Adquirir entrada").click();
+            cy.contains("Auditorio").click();
+            cy.get('button[title="Fila 9, Columna 6"]').should("be.disabled");
+        });
+
+        it("Caso 3: Seleccion de asientos disponibles(butacas)", () => {
+            const asientosSeleccionados = [];
+            const asientosInfo = [];
+            cy.get('[data-cy="btn-ver-evento-4"]').click();
+            cy.contains("Adquirir entrada").click();
+            cy.contains("Con Butacas").click().wait(1000);
+            cy.seleccionarAsientos(2).then((asientosCapturados) => {
+                asientosCapturados.forEach(asiento => lugaresParaLimpiar.push(asiento));
+                cy.validarAsientos(asientosCapturados)
+            });
+            cy.contains("Asientos").should("contain", "Asientos seleccionados: 2");
+           
+
+        });
+
+        it("Caso 4: Seleccion de asientos disponibles(Sin butacas)", () => {
+            cy.get('[data-cy="btn-ver-evento-1"]').click();
+            cy.contains("Adquirir entrada").click();
+            cy.contains("Sin Butacas").click();
+            cy.contains("+").click().click().click();
+            cy.get("span").should("contain", "3");
+            cy.contains("p", "Precio")
+                .invoke("text")
+                .then((textoCapturado) => {
+                    const soloNumeroStr = textoCapturado.replace(/[^0-9]/g, "");
+                    // 2. Convertimos: De texto ("16200") a Número real (16200) para poder sumar/comparar
+                    const precioFinal = parseInt(soloNumeroStr);
+                    cy.log(precioFinal);
+                    let valorTotal = precioFinal * 3;
+                    cy.get("span").should("contain", valorTotal);
+                });
+        });
+
+        it("Caso 5: Seleccion de asientos disponibles(mesas)", () => {
+            cy.get('[data-cy="btn-ver-evento-11"]').click();
+            cy.contains("Adquirir entrada").click();
+            cy.contains("Mesa 5").click();
+            cy.seleccionarAsientos(2).then((asientosCapturados) => {
+                asientosCapturados.forEach(asiento => lugaresParaLimpiar.push(asiento));
+                cy.validarAsientos(asientosCapturados)
+            });
+            cy.contains("Asientos").should("contain", "Asientos seleccionados: 2");
+        });
+
+        it("Caso 6: Chequear disponibilidad por fecha", () => {
+            cy.get('[data-cy="btn-ver-evento-9"]').click();
+            cy.contains("Adquirir entrada").click().wait(1000);
+            cy.contains("span", "sábado, 21 de junio de 2025").click("top");
+            cy.contains("23:00").click();
+            cy.contains("Continuar con la compra").click();
+            cy.contains("Audiotorio").click();
+            cy.seleccionarAsientos(2).then((asientosCapturados) => {
+                asientosCapturados.forEach(asiento => lugaresParaLimpiar.push(asiento));
+                cy.validarAsientos(asientosCapturados)
+            });
+            cy.contains("Asientos").should("contain", "Asientos seleccionados: 2");
+        });
+        // no pude hacerlos con las otras fechas por un tema de diseño, no se si se superponian los div's que no agarraba el del sabado 14, creo que se superponia por el efecto ese de la card con el domingo 15
+        // asi que agarre el ultimo para que pase la prueba, despues veo si lo puedo arreglar
+        // hay que cambiar el test case
+
+
+        it("Caso 8: Valor correcto del precio total", () => {
+            cy.get('[data-cy="btn-ver-evento-2"]').click();
+            cy.contains("Adquirir entrada").click();
+            cy.contains("Con Butacas").click();
+            cy.seleccionarAsientos(2).wait(1000)
+           .then((asientosCapturados) => {
+                asientosCapturados.forEach(asiento => lugaresParaLimpiar.push(asiento));
+            });
+            cy.contains("Comprar").click();
+            cy.contains("2 entradas").should("be.visible");
+            cy.contains("Precio base: $5000");
+            cy.contains("$400.00 (8% del valor base)").should("be.visible");
+            cy.get("div").should("be.visible").and("contain", "$5400");
+            cy.go('back').wait(1000)
+            cy.contains("Con Butacas").click();
+        });
+
+        it("Caso 9: Compra de entrada", () => {
+            cy.get('[data-cy="btn-ver-evento-2"]').click();
+            cy.contains("Adquirir entrada").click();
+            cy.contains("Con Butacas").click();
+            cy.seleccionarAsientos(2)
+            cy.contains("Comprar").click();
+            cy.get("input").check({ force: true });
+            cy.window().then((win) => {
+                // 1. Creamos el espía (stub) sobre la función 'open'
+                cy.stub(win, "open").as("abrir");
+            });
+            cy.contains("Pagar").click();
+            cy.get("@abrir").should(
+                "be.calledWith",
+                Cypress.sinon.match("mercadopago.com")
+            );
+        });
+        it("Caso 10: Compra de entrada(Sin butaca)", () => {
+            cy.get('[data-cy="btn-ver-evento-2"]').click();
+            cy.contains("Adquirir entrada").click();
+            cy.contains("Sin Butacas").click();
+            cy.contains("+").click().click();
+            cy.contains("Comprar").click().wait(2000);
+            cy.get("input").check({ force: true });
+            cy.window().then((win) => {
+                // 1. Creamos el espía (stub) sobre la función 'open'
+                cy.stub(win, "open").as("abrir");
+            });
+            cy.contains("Pagar").click();
+            cy.get("@abrir").should(
+                "be.calledWith",
+                Cypress.sinon.match("mercadopago.com")
+            );
+        });
+
+        it("Caso 11: Compra de entrada(Con mesas)", () => {
+            cy.get('[data-cy="btn-ver-evento-11"]').click();
+            cy.contains("Adquirir entrada").click();
+            cy.contains("Mesa 5").click();
+            cy.seleccionarAsientos(2)
+            cy.contains("Comprar").click().wait(1000);
+            cy.get("input").check({ force: true });
+            cy.window().then((win) => {
+                // 1. Creamos el espía (stub) sobre la función 'open'
+                cy.stub(win, "open").as("abrir");
+            });
+            cy.contains("Pagar").click();
+            cy.get("@abrir").should(
+                "be.calledWith",
+                Cypress.sinon.match("mercadopago.com")
+            );
+        });
+
+        it("Caso 15: Informacion de Terminos y condiciones", () => {
+            cy.get('[data-cy="btn-ver-evento-1"]').click();
+            cy.contains("Adquirir entrada").click();
+            cy.contains("Con Butacas").click();
+            cy.seleccionarAsientos(2).then((asientosCapturados) => {
+                asientosCapturados.forEach(asiento => lugaresParaLimpiar.push(asiento));
+            });
+            cy.contains("Comprar").click();
+            cy.contains("términos y condiciones.").click()
+            cy.contains('Términos y Condiciones de Compra - Ticketazo')
+                .should('be.visible')
+            cy.contains('ROL DE TICKETAZO').should('be.visible')
+            cy.contains('Cerrar').click()
+            cy.go('back')
+            cy.contains("Con Butacas").click()
+        });
+
+        //para este test, al no poder eliminar los lugares comprados
+        //cada vez que ejecute el test me va a dar error a menos
+        //que cambie las coordenadas, asi que lo dejo en skip
+        it.skip("Caso 16: Compra entrada gratis", () => {
+            cy.get('input[type="search"]').type('G2D')
+            cy.get('[data-cy="btn-ver-evento-466"]').click();
+            cy.contains("Adquirir entrada").click();
+            cy.contains("General").click();
+            cy.seleccionarAsientos(1)
+            cy.contains("Comprar").click();
+            cy.contains('Generar Entrada Gratuita').click()
+            cy.url().should('include', '/tickets/list');
+        });
+
     });
-  });
-  it("Caso 1: Chequear disponibilidad de lugares", () => {
-    cy.get('[data-cy="btn-ver-evento-8"]').click();
-    cy.contains("Adquirir entrada").click();
-    cy.contains("Auditorio").click();
-    cy.get('button[title="Fila 9, Columna 6"]').should("be.disabled");
-  });
-
-  it("Caso 3: Seleccion de asientos disponibles(butacas)", () => {
-    cy.get('[data-cy="btn-ver-evento-4"]').click();
-    cy.contains("Adquirir entrada").click();
-    cy.contains("Con Butacas").click();
-    cy.get('button[title="Fila 8, Columna 15"]').click();
-    cy.get('button[title="Fila 8, Columna 16"]').click();
-    cy.contains("F8 C15").should("be.exist");
-    cy.contains("F8 C16").should("be.exist");
-    cy.contains("Asientos").should("contain", "Asientos seleccionados: 2");
-  });
-
-  it("Caso 4: Seleccion de asientos disponibles(Sin butacas)", () => {
-    cy.get('[data-cy="btn-ver-evento-1"]').click();
-    cy.contains("Adquirir entrada").click();
-    cy.contains("Sin Butacas").click();
-    cy.contains("+").click().click().click();
-    cy.get("span").should("contain", "3");
-    cy.contains("p", "Precio")
-      .invoke("text")
-      .then((textoCapturado) => {
-        const soloNumeroStr = textoCapturado.replace(/[^0-9]/g, "");
-        // 2. Convertimos: De texto ("16200") a Número real (16200) para poder sumar/comparar
-        const precioFinal = parseInt(soloNumeroStr);
-        cy.log(precioFinal);
-        let valorTotal = precioFinal * 3;
-        cy.get("span").should("contain", valorTotal);
-      });
-  });
-
-  it("Caso 5: Seleccion de asientos disponibles(mesas)", () => {
-    cy.get('[data-cy="btn-ver-evento-11"]').click();
-    cy.contains("Adquirir entrada").click();
-    cy.contains("Mesa 5").click();
-    cy.get('button[title="Fila 1, Columna 4"]').click();
-    cy.get('button[title="Fila 1, Columna 5"]').click();
-    cy.contains("F1 C4").should("be.exist");
-    cy.contains("F1 C5").should("be.exist");
-    cy.contains("Asientos").should("contain", "Asientos seleccionados: 2");
-  });
-
-  it("Caso 6: Chequear disponibilidad por fecha", () => {
-    cy.get('[data-cy="btn-ver-evento-9"]').click();
-    cy.contains("Adquirir entrada").click().wait(1000);
-    cy.contains("span", "sábado, 21 de junio de 2025").click("top");
-    cy.contains("23:00").click();
-    cy.contains("Continuar con la compra").click();
-    cy.contains("Audiotorio").click();
-    cy.get('button[title="Fila 5, Columna 7"]').click();
-    cy.get('button[title="Fila 5, Columna 8"]').click();
-    cy.contains("F5 C7").should("be.exist");
-    cy.contains("F5 C8").should("be.exist");
-    cy.contains("Asientos").should("contain", "Asientos seleccionados: 2");
-  });
-  // no pude hacerlos con las otras fechas por un tema de diseño, no se si se superponian los div's que no agarraba el del sabado 14, creo que se superponia por el efecto ese de la card con el domingo 15
-  // asi que agarre el ultimo para que pase la prueba, despues veo si lo puedo arreglar
-  // hay que cambiar el test case
-
-
-  it("Caso 8: Valor correcto del precio total", () => {
-    cy.get('[data-cy="btn-ver-evento-2"]').click();
-    cy.contains("Adquirir entrada").click();
-    cy.contains("Con Butacas").click();
-    cy.get('button[title="Fila 5, Columna 18"]').click().wait(2000);
-    cy.get('button[title="Fila 5, Columna 19"]').click().wait(2000);
-    cy.contains("Comprar").click();
-    cy.contains("2 entradas").should("be.visible");
-    cy.contains("Precio base: $5000");
-    cy.contains("$400.00 (8% del valor base)").should("be.visible");
-    cy.get("div").should("be.visible").and("contain", "$5400");
-  });
-
-  it("Caso 9: Compra de entrada", () => {
-    cy.get('[data-cy="btn-ver-evento-2"]').click();
-    cy.contains("Adquirir entrada").click();
-    cy.contains("Con Butacas").click();
-    cy.get('button[title="Fila 5, Columna 18"]').click().wait(1000);
-    cy.get('button[title="Fila 5, Columna 19"]').click().wait(1000);
-    cy.contains("Comprar").click();
-    cy.get("input").check({ force: true });
-    cy.window().then((win) => {
-      // 1. Creamos el espía (stub) sobre la función 'open'
-      cy.stub(win, "open").as("abrir");
-    });
-    cy.contains("Pagar").click();
-    cy.get("@abrir").should(
-      "be.calledWith",
-      Cypress.sinon.match("mercadopago.com")
-    );
-  });
-  it("Caso 10: Compra de entrada(Sin butaca)", () => {
-    cy.get('[data-cy="btn-ver-evento-2"]').click();
-    cy.contains("Adquirir entrada").click();
-    cy.contains("Sin Butacas").click();
-    cy.contains("+").click().click();
-    cy.contains("Comprar").click().wait(2000);
-    cy.get("input").check({ force: true });
-    cy.window().then((win) => {
-      // 1. Creamos el espía (stub) sobre la función 'open'
-      cy.stub(win, "open").as("abrir");
-    });
-    cy.contains("Pagar").click();
-    cy.get("@abrir").should(
-      "be.calledWith",
-      Cypress.sinon.match("mercadopago.com")
-    );
-  });
-
-  it("Caso 11: Compra de entrada(Con mesas)", () => {
-    cy.get('[data-cy="btn-ver-evento-11"]').click();
-    cy.contains("Adquirir entrada").click();
-    cy.contains("Mesa 5").click();
-    cy.get('button[title="Fila 1, Columna 4"]').click();
-    cy.get('button[title="Fila 1, Columna 5"]').click();
-    cy.contains("Comprar").click().wait(1000);
-    cy.get("input").check({ force: true });
-    cy.window().then((win) => {
-      // 1. Creamos el espía (stub) sobre la función 'open'
-      cy.stub(win, "open").as("abrir");
-    });
-    cy.contains("Pagar").click();
-    cy.get("@abrir").should(
-      "be.calledWith",
-      Cypress.sinon.match("mercadopago.com")
-    );
-  });
-
-    it("Caso 15: Informacion de Terminos y condiciones", () => {
-    cy.get('[data-cy="btn-ver-evento-1"]').click();
-    cy.contains("Adquirir entrada").click();
-    cy.contains("Con Butacas").click();
-    cy.get('button[title="Fila 4, Columna 11"]').click().wait(1000);
-    cy.get('button[title="Fila 4, Columna 12"]').click().wait(1000);
-    cy.contains("Comprar").click();
-    cy.contains("términos y condiciones.").click()
-    cy.contains('Términos y Condiciones de Compra - Ticketazo')
-    .should('be.visible')
-    cy.contains('ROL DE TICKETAZO').should('be.visible')
-  });
-
-    //para este test, al no poder eliminar los lugares comprados
-    //cada vez que ejecute el test me va a dar error a menos
-    //que cambie las coordenadas, asi que lo dejo en skip
-    it.skip("Caso 16: Compra sin seleccionar cantidad de entradas", () => {
-    cy.get('input[type="search"]').type('G2D')
-    cy.get('[data-cy="btn-ver-evento-466"]').click();
-    cy.contains("Adquirir entrada").click();
-    cy.contains("General").click();
-    cy.get('button[title="Fila 1, Columna 2"]').click().wait(1000);
-    cy.contains("Comprar").click();
-    cy.contains('Generar Entrada Gratuita').click()
-    cy.url().should('include', '/tickets/list');
-    });
-    
-});
 })
 
 
